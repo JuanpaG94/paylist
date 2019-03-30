5dca08786d1fc81d0f9a67f8dad786ffcd109ae7
Juan <jpaglez@gmail.com>
2019-03-30 18:43:51 +0100

    New project. Firebase integration, login & register implemented

---
+++ paylist_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'auth.dart';
+import 'root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return new MaterialApp(
+      title: 'Paylist',
+      theme: new ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: new RootPage(auth: new Auth())
+    );
+  }
+}
