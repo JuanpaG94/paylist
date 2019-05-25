fc0b63703c403cc49d5cd2135c81eae790e2a837
Juan Pablo <jpaglez@gmail.com>
2019-05-25 21:57:34 +0200

    Update gitignore

--- App.js
+++ App.js
@@ -21,8 +21,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Main screen</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>Welcome to Paylist</Text>
+        <Text style={styles.instructions}>This is the main screen</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
